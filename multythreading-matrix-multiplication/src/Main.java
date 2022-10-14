07b8dc68e4607d025112a5bd30574e1e91c464fb
Mariia <coldrainLV@gmail.com>
2022-10-14 12:36:14 +0300

    Matrix multiplication in multi threads, contains many redundant code to model load situation

--- multythreading-matrix-multiplication/src/Main.java
+++ multythreading-matrix-multiplication/src/Main.java
@@ -6,25 +6,48 @@ public class Main {
 
   public static void main(String[] args) throws ExecutionException, InterruptedException {
     MatrixMultiplicator multiplicator = new MatrixMultiplicator();
-    System.out.println("Number of processors " + Runtime.getRuntime().availableProcessors());
-    int[][] valuesForLeftMatrix = {{5, 2, 3}, {3, 4, 3}, {3, 4, 5}, {3, 4, 5}, {5, 2, 3}, {3, 4, 3}, {3, 4, 5}, {3, 4, 5}};
+    int processors = Runtime.getRuntime().availableProcessors();
+    System.out.println("Number of processors " + processors);
+    int[][] valuesForLeftMatrix =
+        multiplicator.generateRandomMatrix(2,100000000);
 
-    int[][] valuesForRightMatrix = {{1, 2, 3, 4, 3, 5, 3, 5}, {1, 2, 3, 4, 3, 5, 3, 5}, {1, 2, 3, 4, 3, 5, 3, 5}};
+    int[][] valuesForRightMatrix =
+        multiplicator.generateRandomMatrix(100000000, 2);
 
     Matrix leftMatrix = new Matrix(valuesForLeftMatrix.length,valuesForLeftMatrix[0].length);
     leftMatrix.setValues(valuesForLeftMatrix);
     Matrix rightMatrix = new Matrix(valuesForRightMatrix.length,valuesForRightMatrix[0].length);
     rightMatrix.setValues(valuesForRightMatrix);
 
+    long start = System.nanoTime();
     int[][] result = multiplicator.multiply(leftMatrix, rightMatrix);
+    long finish = System.nanoTime();
+    float parallelTime = (finish - start) / 1_000_000_000f;
+    System.out.println("Parallel multiplication consumed " + parallelTime + " seconds");
 
-    System.out.println("\nresulting matrix:");
-    for (int i = 0; i < result.length; i++) {
-      for( int j = 0; j < result[i].length; j++) {
-        System.out.print( result[i][j] + " ");
-      }
-      System.out.println();
-    }
+    long start1 = System.nanoTime();
+    int[][] result1 = multiplicator.sequentialMultiply(leftMatrix, rightMatrix);
+    long finish1 = System.nanoTime();
+    float sequentialTime = (finish1 - start1) / 1_000_000_000f;
+    System.out.println("Sequential multiplication consumed " + sequentialTime + " seconds");
+
+    float speedUp = sequentialTime / parallelTime;
+
+    System.out.println("SpeedUP = " + speedUp);
+    System.out.println("Efficiency = " + speedUp / processors * 100 +"%");
+
+//      System.out.println("\nresulting matrix:");
+//      for (int i = 0; i < result.length; i++) {
+//        for (int j = 0; j < result[i].length; j++) {
+//          if(result[i][j] == result1[i][j] ) {
+//            System.out.print(result[i][j] + " ");
+//          } else {
+//            throw new RuntimeException("Results of calculation are wrong");
+//          }
+//        }
+//        System.out.println();
+//      }
+
   }
 
 }
--- multythreading-matrix-multiplication/src/service/MatrixMultiplicator.java
+++ multythreading-matrix-multiplication/src/service/MatrixMultiplicator.java
@@ -1,11 +1,11 @@
 package service;
 
+import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import model.Element;
 import model.Matrix;
 
 public class MatrixMultiplicator {
@@ -14,19 +14,32 @@ public class MatrixMultiplicator {
   private final ExecutorService executorService = Executors.newFixedThreadPool(availableThreads);
 
   public int[][] sequentialMultiply(Matrix leftMatrix, Matrix rightMatrix) {
-    //TODO
-    return null;
+    System.out.println("Sequential multiplication have started");
+    int[][] result = new int[leftMatrix.getRows()][rightMatrix.getColumns()];
+    for (int i = 0; i < leftMatrix.getRows(); i++) {
+      for(int j = 0; j < rightMatrix.getColumns(); j++ ) {
+
+        for (int k = 0; k < leftMatrix.getColumns(); k++) {
+          result [i][j] += leftMatrix.getValues()[i][k] * rightMatrix.getValues()[k][j];
+        }
+        for(int c = 0 ; c < 1_000_000_00; c++) {
+          System.nanoTime();
+        }
+      }
+    }
+
+    return result;
   }
 
   public int[][] multiply(Matrix leftMatrix, Matrix rightMatrix)
       throws InterruptedException, ExecutionException {
+    System.out.println("Parallel multiplication have started");
 
     if (leftMatrix.getColumns() != rightMatrix.getRows()) {
       throw new RuntimeException("Wrong dimension of matrices");
     }
     CountDownLatch latch = new CountDownLatch(leftMatrix.getRows() * rightMatrix.getColumns());
-    Element[][] resultMatrix = new Element[leftMatrix.getRows()][rightMatrix.getColumns()];
-    long start = System.nanoTime();
+    Future<Integer>[][] resultMatrix = new Future[leftMatrix.getRows()][rightMatrix.getColumns()];
     for(int i = 0; i < leftMatrix.getRows(); i++) {
       for(int j =0; j < rightMatrix.getColumns(); j++) {
         int[] row = new int[leftMatrix.getColumns()];
@@ -38,37 +51,44 @@ public class MatrixMultiplicator {
         for(int m = 0; m < rightMatrix.getRows(); m++) {
           column[m] = rightMatrix.getValues()[m][j];
         }
-        Element futureElement = new Element();
-
-        futureElement.setValue(executorService.submit( () -> {
+        resultMatrix[i][j] = (executorService.submit( () -> {
           System.out.println(Thread.currentThread().getName() + " received a task");
             int element = this.multiplyRowByColumn(row,column);
             latch.countDown();
             return element;
             })
         );
-
-        resultMatrix[i][j] = futureElement;
       }
     }
     executorService.shutdown();
     latch.await();
-    long finish = System.nanoTime();
-    System.out.println("time consumed: " + (finish - start) / 1_000_000_000f + " seconds");
     int result[][] = new int[leftMatrix.getRows()][rightMatrix.getColumns()];
     for(int i = 0; i < leftMatrix.getRows(); i++) {
       for(int j =0; j < rightMatrix.getColumns(); j++) {
-        result[i][j] = resultMatrix[i][j].getValue().get();
+        result[i][j] = resultMatrix[i][j].get();
       }
     }
 
     return result;
   }
 
+  public int[][] generateRandomMatrix(int numberOfRows, int numberOfColumns) {
+    int[][] matrix = new int[numberOfRows][numberOfColumns];
+    Random random = new Random();
+    for(int i = 0; i < numberOfRows; i++ ) {
+      for(int j = 0; j < numberOfColumns; j++) {
+        matrix[i][j] = random.nextInt(10);
+      }
+    }
+    return matrix;
+  }
   private int multiplyRowByColumn(int[] row, int[] column) {
     int sum = 0;
     for(int i = 0; i < row.length; i++) {
       sum += row[i] * column[i];
+    }
+    for(int c = 0 ; c < 1_000_000_00; c++) {
+      System.nanoTime();
     }
     return sum;
   }
